c2850323d87e8dbf895dc58b6047e23a537c7849
Hironobu Nishikokura <nishiko@mindia.jp>
2016-05-14 18:03:37 +0900

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,15 +10,15 @@ class WelcomeController < ApplicationController
     to = params['result'].first['content']['from']
 
     if text.nil?
-      Line.echo to, '24分集中開始！'
-      1.upto(23).each do |i|
-        sleep 1 * 60
-        Line.echo to, "あと#{24-i}分！"
-      end
-      sleep 1 * 60
-      Line.echo to, '終了です！お疲れ様でした。５分間ゆっくり休んでください♡'
-      sleep (5 * 60) 
-      Line.echo to, '休憩時間終了です。また24分頑張ってくださいっ！' 
+      #Line.echo to, '24分集中開始！'
+      #1.upto(23).each do |i|
+      #  sleep 1 * 60
+      #  Line.echo to, "あと#{24-i}分！"
+      #end
+      #sleep 1 * 60
+      #Line.echo to, '終了です！お疲れ様でした。５分間ゆっくり休んでください♡'
+      #sleep (5 * 60) 
+      #Line.echo to, '休憩時間終了です。また24分頑張ってくださいっ！' 
     else
       disp = "「#{text}」を開始するのですね！"
       pres = Worklog.where(user_key: to).order('created_at desc')
@@ -29,13 +29,10 @@ class WelcomeController < ApplicationController
         remain = (sec - hour * 60 * 60)
         min  = (remain / 60 ).to_i
         sec  = remain - 60*min
-        if hour > 0
-          duration = "#{hour}時間#{min}分#{sec}秒"
-        elsif min > 0
-          duration = "#{min}分#{sec}秒"
-        else
-          duration = "#{sec}秒"
-        end
+        duration = ''
+        duration += "#{hour}時間" if hour > 0
+        duration += "#{min}分" if min > 0
+        duration += "#{sec}秒"
         disp += "\n前回の「#{pre.name}」は#{duration}でした"
       end
 
