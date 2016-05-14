bff3a05bb907726b5622aed50b383dfac941bc0e
Hironobu Nishikokura <nishiko@mindia.jp>
2016-05-14 16:18:04 +0900

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,8 @@ class WelcomeController < ApplicationController
     text = params['result'].first['content']['text']
     to = params['result'].first['content']['from']
 
+    raise to.inspect
+
     if text.nil?
       Line.echo to, '24分集中開始！'
       1.upto(23).each do |i|
